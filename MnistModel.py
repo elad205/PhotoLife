b8bd7f3f918b2683750c652c5cefadd293271373
Elad Cynamon <eladcyn@gmail.com>
2019-01-05 19:02:02 +0200

    fixed the network and changed architecture to work on pytorch optimizer

--- MnistModel.py
+++ MnistModel.py
@@ -1,128 +1,192 @@
-import mnist
 import numpy as np
 import torch
 import visdom
-import VisdomServer
 import math
-import torchvision
+import torchvision.datasets.mnist
+import copy
 
 
 class Layer:
+    """
+    this class represents a layer in the network, it contains the
+    layer tensor and the place of the layer in the network
+    """
     def __init__(self, layer_tensor, layer_number):
         self.layer = layer_tensor
         self.index = layer_number
 
 
-class NeuralNetwork:
-    def __init__(self, input_layer, output_label):
-
-        # using the nvidia cuda api in order to perform calculations on a GPU
-        device = torch.device('cuda')
-        self.cost = (0, 0)
-        self.rate = 1e-6
-
-        # initialize the weights - 4 layers total
-        self.weights = []
-        self.weights.append(torch.randn(784, 2500, device=device, dtype=torch
-                                        .double, requires_grad=True))
-        self.weights.append(torch.randn(2500, 1000, device=device, dtype=torch
-                                        .double, requires_grad=True))
-        self.weights.append(torch.randn(1000, 500, device=device, dtype=torch
-                                        .double, requires_grad=True))
-        self.weights.append(torch.randn(500, 10,  device=device, dtype=torch
-                                        .double, requires_grad=True))
-
-        for index in range(0, len(input_layer), 100):
-            feature = input_layer[index: index + 100]
-            output_number = output_label[index: index + 100]
-            prediction_layer = self.forward_process(
-                Layer(self.parse_python_input_to_cuda_tensor(feature, 100, 784), 0))
-            loss = self.softmax_loss(prediction_layer.layer,
-                                     self.convert_label(output_number))
-            print(loss)
-            self.back_propagation(loss)
-
-    def forward_process(self, layer):
+class NeuralNetwork(torch.nn.Module):
+    def __init__(self, viz_tool, layer_number):
         """
-        this is the forward iteration of the network
-        :param layer:
+        initialize the network variables
+        :param viz_tool: visdom object to display plots
+        :param layer_number: the number of layers in the network
+        """
+        super(NeuralNetwork, self).__init__()
+        # using the nvidia cuda api in order to perform calculations on a GPU
+        with torch.no_grad():
+            self.device = torch.device('cuda')
+            self.rate = 0.001
+            self.loss_plot = []
+            self.cost_plot = []
+            self.viz = viz_tool
+            self.network_layers = layer_number
+            self.weights = torch.nn.ModuleList()
+        # initiate the weights
+        self.init_weights_linear_profile([(784, 2500), (2500, 1000),
+                                          (1000, 500), (500, 10)])
+
+        # create an optimizer for the network
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=self.rate)
+
+    def train_model(self, epoches, train_data):
+        """
+        train the model
+        :param epoches: the number of epoches to perform
+        :param train_data: the dataset
         :return:
         """
+        loss = None
+        for epoch in range(epoches):
+            # run on all the data examples parsed by pytorch vision
+            for i, (images, labels) in enumerate(train_data):
+                # flatten the image to 1d tensor
+                images = images.reshape(-1, 28 * 28).to(self.device)
+                # feed forward through the network
+                prediction_layer = self.forward(Layer(images, 0))
+                # calculate the loss
+                loss = self.softmax_loss(prediction_layer.layer,
+                                         labels.to(self.device))
+                # backpropagate through the network
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                # save data for plots
+                self.loss_plot.append(i)
+                self.loss_plot.append(loss.item())
+            print(loss)
+            # create a graph of the loss in perspective to the iterations
+            graph = np.array(self.loss_plot).\
+                reshape(int(len(self.loss_plot)/2), 2)
+            if epoch == 1:
+                self.viz.scatter(graph)
+            # zero the data
+            self.loss_plot = []
+            # every epoch calculate the averge loss
+            self.cost_plot.append(epoch)
+            self.cost_plot.append(np.mean(graph[1]))
+        # create a graph of the cost in respect to the ephoces
+        graph = np.array(self.cost_plot).reshape(
+            int(len(self.cost_plot) / 2), 2)
+        self.viz.scatter(graph)
+
+    def forward(self, layer):
+        """
+               this is the forward iteration of the network
+               :param layer: a layer object
+               :return: the prediction layer
+               """
         if layer.index == len(self.weights):
             return layer
         else:
             new_layer = self.create_activated_layer(layer)
-            layer_1 = self.forward_process(new_layer)
+            layer_1 = self.forward(new_layer)
             return layer_1
 
     def create_activated_layer(self, layer):
         """
         creates activated layer using the relu function
         :param layer: layer objects
-        :return: activated layer
+        :return: the next layer of the network
         """
         # create the layer of the model
-        calculated_layer = layer.layer.mm(self.weights[layer.index])
+        calculated_layer = layer.layer.matmul(
+            self.weights[layer.index].weight.t()) + self.weights[layer.index].\
+            bias
         # perform the activation function on every neuron [relu]
-        activated_layer = calculated_layer.clamp(min=0)
-        return Layer(activated_layer, layer.index + 1)
-
-    @staticmethod
-    def parse_python_input_to_cuda_tensor(lst, batch_size, input_size):
-        """
-        this function gets a python list and parses it into a pytorch tensor
-        :param lst:  the python list
-        :param batch_size: the batch size of the input
-        :param input_size: the size of each input
-        :return: parsed pytorch tensor
-        """
-        temp = np.array(lst).reshape(batch_size, input_size)
-        temp = temp.astype('float')
-        torch_tensor = torch.from_numpy(temp)
-        torch_tensor = torch_tensor.to(torch.device('cuda'))
-        return torch_tensor
+        # don't activate the prediction layer
+        if layer.index < len(self.weights) - 1:
+            activated_layer = calculated_layer.clamp(min=0)
+        else:
+            activated_layer = calculated_layer
+        return Layer(layer_tensor=activated_layer,
+                     layer_number=layer.index + 1)
 
     @staticmethod
     def softmax_loss(prediction_layer, expected_output):
+        """
+        calculate the nll loss of the network
+        :param prediction_layer: the predication layer
+        :param expected_output: the loss of the model
+        :return:the loss
+        """
+        # softmax + log is better than performing the actions separately
         loss_softmax = torch.nn.CrossEntropyLoss()
-        # currently does loss calculation in the cpu in order to
-        # avoid gradient explosion
-        prediction_layer = prediction_layer.to(torch.device('cpu'))
         loss = loss_softmax(prediction_layer, expected_output)
         return loss
 
-    def back_propagation(self, loss):
-        # perform the backpropagation on the network
-        torch.autograd.backward(loss)
+    def init_weights_linear_profile(self, sizes):
+        """
+        this profile creates the weights of the network
+        :param sizes: the size of the network
+        :return:
+        """
+        for index in range(self.network_layers):
+            self.weights.append(torch.nn.Linear(
+                sizes[index][0], sizes[index][1]).to(self.device))
 
+    def test_model(self, test_data):
+        total = 0
+        correct = 0
+        cost = []
         with torch.no_grad():
-            for index in range(len(self.weights)):
-                self.weights[index] -= self.rate * self.weights[index].grad
-                self.weights[index].grad.zero_()
+            for i, (images, labels) in enumerate(test_data):
+                images = images.reshape(-1, 28 * 28).to(self.device)
+                prediction_layer = self.forward(Layer(images, 0)).layer
+                _, predicted = torch.max(prediction_layer.data, 1)
+                labels = labels.to(torch.device('cuda'))
+                total += labels.size(0)
+                cost.append(i)
+                cost.append(self.softmax_loss(prediction_layer, labels).item())
+                correct += (predicted == labels).sum().item()
+            graph = np.array(cost).reshape(int(len(cost) / 2), 2)
+            self.viz.scatter(graph)
+            print(correct / 100)
 
-    @staticmethod
-    def convert_label(label):
-        output_number = np.array(label)
-        output_number = output_number.astype('int64')
-        output_number = torch.from_numpy(output_number)
-        output_number = output_number.to(torch.device('cpu'))
-        return output_number
+
+def load_model(path, vis, layers=4):
+    model = NeuralNetwork(vis, layers)
+    model.load_state_dict(torch.load(path))
+    model.eval()
+    return model
 
 
 def main():
-    ser = VisdomServer.Server()
     vis = visdom.Visdom()
-    vis.text('Hello, world!')
-    data = mnist.MNIST(r"C:\Users\eladc\PycharmProjects\PyTorchEnv\MNIST")
-    images, labels = data.load_training()
-    print(labels[0])
-    # print(np.asarray(images)[0].reshape(28,28))
-    img = np.array(images[0]).astype('uint8').reshape(1, 28, 28)
-    vis.image(img)
-    img = np.array(images[0]).reshape(-1, 784)
-    img = img.astype('float')
-    print(img.dtype)
-    n = NeuralNetwork(images, labels)
+    train = torchvision.datasets.mnist.MNIST(
+        r"..\..\data", train=True, download=True,
+        transform=torchvision.transforms.ToTensor())
+    train_loader = torch.utils.data.DataLoader(dataset=train,
+                                               batch_size=100,
+                                               shuffle=True)
+    test_dataset = torchvision.datasets.MNIST(root=r'..\..\data',
+                                              train=False,
+                                              transform=torchvision.transforms.
+                                              ToTensor())
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=1,
+                                              shuffle=False)
+
+    evalute_network(vis, train_loader, test_loader)
+
+
+def evalute_network(vis, train_loader, test_loader):
+    n = NeuralNetwork(vis, 4)
+    n.train_model(800, train_loader)
+    n.test_model(test_loader)
+    torch.save(n.state_dict(), 'n.ckpt')
 
 
 if __name__ == '__main__':
