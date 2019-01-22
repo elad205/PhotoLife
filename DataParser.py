b94bde1fc4db7fe58d5c98f1f21bc06886ef5ebf
Elad Cynamon <eladcyn@gmail.com>
2019-01-22 16:48:50 +0200

    fixed more pep8 issues and added an option to iterata over data loader object

--- DataParser.py
+++ DataParser.py
@@ -6,11 +6,26 @@ import torchvision
 
 
 class DataParser:
-    def __init__(self, batch_size, width_height):
+    def __init__(self, batch_size):
         self.feature_list = []
         self.label_list = []
         self.batch_size = batch_size
-        self.image_axe = width_height
+
+    def __next__(self):
+        # stop when iterated over all of the objects
+        if self.batch_counter == len(self.feature_list):
+            raise StopIteration
+        # get to the next item
+        self.batch_counter += 1
+
+        # return the images and labels
+        return self.feature_list[self.batch_counter], self.label_list[
+            self.batch_counter]
+
+    def __iter__(self):
+        # create a counter to index the lists
+        self.batch_counter = -1
+        return self
 
     def load_cifar10(self):
         """
@@ -20,22 +35,19 @@ class DataParser:
         test data.
         """
         train_dataset = torchvision.datasets.CIFAR10(
-            "data", train=True, download=True,
-            transform=torchvision.transforms.ToTensor())
+            root="cifar10", train=True,
+            download=True, transform=torchvision.transforms.ToTensor())
 
         # load training data
-        train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                                   batch_size=self.batch_size,
-                                                   shuffle=True)
+        train_loader = torch.utils.data.DataLoader(
+            dataset=train_dataset, batch_size=self.batch_size, shuffle=True)
 
-        test_dataset = torchvision.datasets.CIFAR10(root='data',
-                                                  train=False,
-                                                  transform=torchvision.transforms.
-                                                  ToTensor())
+        test_dataset = torchvision.datasets.CIFAR10(
+            root='cifar10',
+            train=False, transform=torchvision.transforms.ToTensor())
         # load test data
-        test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                                  batch_size=self.batch_size,
-                                                  shuffle=False)
+        test_loader = torch.utils.data.DataLoader(
+            dataset=test_dataset, batch_size=self.batch_size, shuffle=False)
 
         return train_loader, test_loader
 
@@ -47,7 +59,7 @@ class DataParser:
         structure required for skimage to convert the pic.
         after that it returns the arrays to torch tensors and adds them to a
         list.
-        :param data_loader: a dataloader object containing the data
+        :param data_loader: a data loader object containing the data
         :return:
         """
         for i, (images, labels) in enumerate(data_loader):
@@ -63,12 +75,13 @@ class DataParser:
             lab_image = skimage.color.rgb2lab(1.0 / 255 * images)
 
             # extract the features
-            features = lab_image[:, :, :, 0].reshape(
-                self.batch_size, self.image_axe, self.image_axe, 1)
+            a_dim = lab_image.shape[0]
+            b_dim = lab_image.shape[1]
+            c_dim = lab_image.shape[2]
+            features = lab_image[:, :, :, 0].reshape(a_dim, b_dim, c_dim, 1)
 
             # extract the labels
             labels = lab_image[:, :, :, 1:]
-
             # make the label values smaller than 1
             labels = labels / 128
 
@@ -83,9 +96,19 @@ class DataParser:
 
 
 def main():
-    a = DataParser(30, 32)
+    a = DataParser(30)
     im = a.load_cifar10()
     a.parse_data(im[0])
+    b = None
+    # example of how to iterate on the object
+    for images, labels in a:
+        if b is None:
+            b = images
+        try:
+            print(torch.mean(b-images))
+            b = images
+        except RuntimeError:
+            pass
 
 
 if __name__ == '__main__':
