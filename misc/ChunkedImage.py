13a49feefbff8b4f7d709656fa9b7739e0de7862
Elad Cynamon <eladcyn@gmail.com>
2019-02-08 23:20:31 +0200

    organizing into folders

--- misc/ChunkedImage.py
+++ misc/ChunkedImage.py
@@ -4,11 +4,12 @@ from DataParser import DataParser
 
 class InvalidFormatException(Exception):
     def __init__(self):
-        super(InvalidFormatException,self).__init__("image format should be a square and divisible by chunk size")
+        super(InvalidFormatException, self).__init__(
+            "image format should be a square and divisible by chunk size")
 
 
 class ChunkImage(object):
-    def __init__(self, im, chunk_size=8):
+    def __init__(self, im, chunk_size=2):
         self.image = im
         self.chunk_size = chunk_size
         if len(self.image.shape) != 4 \
@@ -19,7 +20,7 @@ class ChunkImage(object):
 
     def __iter__(self):
 
-        self.counter = [0, 0]
+        self.counter = [0, 0, 0]
         return self
 
     def __len__(self):
@@ -31,21 +32,35 @@ class ChunkImage(object):
         if self.counter[0] == self.image.shape[0]:
             raise StopIteration
 
-        ret_value = self.image[self.counter[0]][:,
-                    self.counter[1]: self.counter[1] + self.chunk_size:,
-                    self.counter[1]: self.counter[1] + self.chunk_size]
+        ret_value = self.image[self.counter[0]][
+                    self.counter[2]: self.counter[2] + self.chunk_size,
+                    self.counter[1]: self.counter[1] + self.chunk_size, 0:]
 
         self.counter[1] += self.chunk_size
 
-        if self.counter[1] >= self.image_size:
-            self.counter[0] += 1
+        if self.counter[1] == self.image_size:
+            self.counter[2] += self.chunk_size
             self.counter[1] = 0
+
+        if self.counter[2] == self.image_size:
+            self.counter[2] = 0
+            self.counter[1] = 0
+            self.counter[0] += 1
 
         return ret_value
 
     def __getitem__(self, item):
 
         return self.image[item]
+
+    @staticmethod
+    def generate_objects(data):
+        imr = []
+        for i, (image, lables) in enumerate(data):
+            image = numpy.transpose(image.numpy(), (0, 2, 3, 1))
+            imr.append(ChunkImage(image))
+
+        return imr
 
 
 if __name__ == '__main__':
@@ -56,4 +71,5 @@ if __name__ == '__main__':
 
     for items in imr:
         for chuncks in items:
-            print(chuncks.shape)
+            pass
+            #print(chuncks.shape)
