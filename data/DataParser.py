fdad4589158294d2d5902484e510895656fbfd56
Elad Cynamon <eladcyn@gmail.com>
2019-04-11 20:12:27 +0300

    improved file loading eficcency

--- data/DataParser.py
+++ data/DataParser.py
@@ -6,6 +6,23 @@ from torchvision import transforms
 import progressbar
 
 
+class PlacesDataSet(torchvision.datasets.ImageFolder):
+    def __init__(self, train_dir, transf):
+        super(PlacesDataSet, self).__init__(train_dir, transform=transf)
+
+    def __getitem__(self, index):
+        path = self.samples[index][0]
+        sample = self.loader(path)
+        augment = transforms.Compose([transforms.RandomHorizontalFlip()])
+        sample = augment(sample)
+        sample = numpy.array(sample)
+        feature, label = DataParser.cvt_numpy_image(sample)
+        feature = self.transform(feature)
+        label = self.transform(label)
+
+        return feature, label
+
+
 class DataParser:
     def __init__(self):
         self.feature_list = []
@@ -44,7 +61,7 @@ class DataParser:
         # load training data
         train_loader = torch.utils.data.DataLoader(
             dataset=train_dataset, batch_size=batch_size, shuffle=True,
-            num_workers=8)
+            num_workers=8, pin_memory=True)
 
         test_dataset = torchvision.datasets.CIFAR10(
             root='cifar10',
@@ -60,16 +77,17 @@ class DataParser:
         train_dir = r'C:\data_256'
         test_dir = r'C:\data_test_256'
 
-        train_dataset = torchvision.datasets.ImageFolder(
+        train_dataset = PlacesDataSet(
             train_dir,
-            transforms.Compose([transforms.ToTensor()]))
+            transforms.Compose([
+                                transforms.ToTensor()]))
         test_dataset = torchvision.datasets.ImageFolder(
             test_dir,
             transforms.Compose([transforms.ToTensor()]))
 
         train_loader = torch.utils.data.DataLoader(
             dataset=train_dataset, batch_size=batch_size,
-            shuffle=True, num_workers=8, pin_memory=True)
+            shuffle=True, num_workers=4, pin_memory=True)
 
         test_loader = torch.utils.data.DataLoader(
             dataset=test_dataset, batch_size=batch_size, shuffle=False,
@@ -83,91 +101,8 @@ class DataParser:
             widgets=['parsing data ', progressbar.SimpleProgress(), ' ',
                      progressbar.ETA()],
             max_value=len(
-                data_loader) * 16 if stopper is None else stopper * 16)
+                data_loader) * 4 if stopper is None else stopper * 4)
         return prog
-
-    def parse_data(self, data_loader, stopper=None):
-        """
-        this function parses the data and splits it into features and labels.
-        It does that by converting the image into a numpy array and than
-        performing operations on the array in order to turn it into the
-        structure required for skimage to convert the pic.
-        after that it returns the arrays to torch tensors and adds them to a
-        list.
-        :param data_loader: a data loader object containing the data
-        :param stopper: a number that indicates when to stop loading data
-        to the memory
-        :return:
-        """
-        prog = self.create_loading_bar(data_loader, stopper=stopper)
-
-        for i, (images, labels) in enumerate(data_loader):
-            # used to get only a fraction of the dataset
-            if stopper is not None and i > stopper:
-                break
-            # convert the images into numpy arrays
-            images = images.numpy()
-            # swap the axes because cv2 uses x , x , c format and torch
-            # uses c, x, x format
-            images = images.transpose((0, 2, 3, 1))
-            # perform the casting from rgb to lab and convert the image to
-            # values lower than 1.
-            lab_image = []
-            for image in images:
-                lab_image.append(cv2.cvtColor(image, cv2.COLOR_RGB2LAB))
-            lab_image = numpy.asarray(lab_image)
-            # extract the features
-            a_dim = lab_image.shape[0]
-            b_dim = lab_image.shape[1]
-            c_dim = lab_image.shape[2]
-            features = lab_image[:, :, :, 0].reshape(a_dim, b_dim, c_dim, 1)
-            # convert the structure back to c,x,x
-            features = features.transpose((0, 3, 1, 2))
-            # extract the labels
-            labels = lab_image[:, :, :, 1:]
-
-            # make the label values smaller than 1
-            labels = labels / 128
-            # convert the structure back to c,x,x
-            labels = labels.transpose((0, 3, 1, 2))
-            # convert the data back to torch format
-            features = torch.from_numpy(features)
-            labels = torch.from_numpy(labels)
-            prog.update(i * a_dim)
-            # append the data to the objects
-            self.feature_list.append(features)
-            self.label_list.append(labels)
-            del lab_image
-
-        prog.finish()
-
-    def rgb_parse(self, data_loader, stopper=None):
-        prog = self.create_loading_bar(data_loader, stopper)
-
-        for i, (images, _) in enumerate(data_loader):
-
-            if stopper is not None and i > stopper:
-                break
-
-            images = images.numpy()
-            images = images.transpose((0, 2, 3, 1))
-            gray_images = []
-            for image in images:
-                gray_images.append(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY))
-
-            gray_images = numpy.asarray(gray_images)
-            a_dim = gray_images.shape[0]
-            b_dim = gray_images.shape[1]
-            c_dim = gray_images.shape[2]
-
-            gray_images = gray_images.reshape((a_dim, b_dim, c_dim, 1))
-            self.feature_list.append(
-                torch.from_numpy(gray_images.transpose((0, 3, 1, 2))))
-            self.label_list.append(torch.from_numpy(images.transpose(
-                0, 3, 1, 2)))
-            prog.update(i * a_dim)
-
-        prog.finish()
 
     @staticmethod
     def convert_to_lab(image_batch, opposite=True):
@@ -207,6 +142,18 @@ class DataParser:
             (0, 3, 1, 2))
 
     @staticmethod
+    def cvt_numpy_image(image):
+        image = image / 255.0
+        image = image.astype('float32')
+        lab_image = cv2.cvtColor(image, cv2.COLOR_RGB2Lab)
+        gray_image = lab_image[:, :, 0]
+        gray_image = gray_image / 100
+        lab_image[:, :, 0] /= 50
+        lab_image[:, :, 0] -= 1
+        lab_image[:, :, 1:] /= 127
+        return gray_image, lab_image
+
+    @staticmethod
     def reconstruct_image(feature, result):
         result = result * 128
         f = feature.cpu().clone().detach().numpy()
@@ -221,6 +168,76 @@ class DataParser:
 
         return numpy.asarray(lst).transpose((0, 3, 1, 2))
 
+    @staticmethod
+    def torch_rgb_to_lab(image_batch, device):
+
+        # convert the images to a large rgb image
+        all_pixels = image_batch.view((-1, 3)).to(device)
+        all_pixels = all_pixels.type(torch.float32)
+
+        # create a mask for pixels which values are smaller than 0.4045
+        linear_mask = all_pixels <= 0.04045
+        linear_mask = linear_mask.type(torch.float32)
+
+        # create a mask for pixels which values are larger than 0.4045
+        exp_mask = all_pixels > 0.04045
+        exp_mask = exp_mask.type(torch.float32)
+
+        pixels = (all_pixels / 12.92 * linear_mask) + (
+                ((all_pixels + 0.55) / 1.055) ** 2.4) * exp_mask
+
+        conv_xyz = torch.Tensor([
+            #    X        Y          Z
+            [0.412453, 0.212671, 0.019334],  # R
+            [0.357580, 0.715160, 0.119193],  # G
+            [0.180423, 0.072169, 0.950227],  # B
+        ]).to(device)
+
+        # perform the transformation
+        xyz_pixels = torch.matmul(pixels, conv_xyz)
+
+        # normalize the pixels to d65
+        xyz_pixels[:, 0] *= 1 / 0.950456
+        xyz_pixels[:, 2] *= 1 / 1.088754
+
+        epsilon = 6 / 29
+
+        # create a linear mask for the pixels
+        linear_mask = xyz_pixels <= epsilon ** 3
+        linear_mask = linear_mask.type(torch.float32)
+
+        # create an exponential mask for the pixels
+        exp_mask = xyz_pixels > epsilon ** 3
+        exp_mask = exp_mask.type(torch.float32)
+
+        # calculate the values according to the formula
+        fxfyfz_pixels = (xyz_pixels / (
+                    3 * epsilon ** 2) + 4 / 29) * linear_mask + (
+                                xyz_pixels ** (1 / 3)) * exp_mask
+
+        conv_lab = torch.Tensor([
+            #  l       a       b
+            [0.0, 500.0, 0.0],       # fx
+            [116.0, -500.0, 200.0],  # fy
+            [0.0, 0.0, -200.0],      # fz
+        ]).to(device)
+
+        # perform the transformation
+        lab_pixels = torch.matmul(fxfyfz_pixels, conv_lab) + torch.Tensor(
+            [-16.0, 0.0, 0.0]).to(device)
+
+        final_images = lab_pixels.view(image_batch.size())
+
+        final_images[:, :, :, 0] /= 50
+        final_images[:, :, :, 0] -= 1
+        final_images[:, :, :, 1:] /= 121
+
+        return DataParser.torch_lab_to_gray(final_images, device), final_images
+
+    @staticmethod
+    def torch_lab_to_gray(image_batch, device):
+        image_batch_gray = image_batch[:, 0, :, :].unsqueeze(1)
+        return image_batch_gray.to(device)
 
 def main():
     ob = DataParser()
