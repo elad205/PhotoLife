8c359e9b3208beb77c6883496cdbe37006f84cc1
Elad Cynamon <eladcyn@gmail.com>
2019-05-14 18:04:17 +0300

    unstaged data norm

--- model/src/colorization/DataParser.py
+++ model/src/colorization/DataParser.py
@@ -17,22 +17,13 @@ class PlacesDataSet(torchvision.datasets.ImageFolder):
         sample = self.loader(path)
         augment = transforms.Compose([
             transforms.RandomHorizontalFlip()])
-        normilize = transforms.Compose(
-            [transforms.ToTensor(),
-             transforms.Normalize(
-                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
         sample = augment(sample)
-
-        if self.norm:
-            sample = normilize(sample)
-            sample = sample.numpy()
-            sample = sample.transpose((1, 2, 0))
-        else:
-            sample = numpy.array(sample)
-
+        sample = numpy.array(sample)
         feature, label = DataParser.rgb_parse(sample)
-        feature = self.transform(feature)
+
         label = self.transform(label)
+        feature = self.transform(feature)
         return feature, label
 
 
@@ -68,7 +59,7 @@ class DataParser:
         test_dataset = PlacesDataSet(
             test_dir,
             transforms.Compose([
-                transforms.ToTensor()]), norm=False)
+                transforms.ToTensor()]), norm=True)
 
         test_loader = torch.utils.data.DataLoader(
             dataset=test_dataset, batch_size=batch_size, shuffle=False,
@@ -97,6 +88,7 @@ class DataParser:
         rgb_image = (image / 255.0).astype('float32')
         gray_image = cv2.cvtColor(image.astype('float32'),
                                   cv2.COLOR_RGB2GRAY) / 255.0
+
         return gray_image, rgb_image
 
     @staticmethod
